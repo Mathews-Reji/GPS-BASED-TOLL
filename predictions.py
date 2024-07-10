80cf9f9b17959ef062e64cc31aecfcc7266b4f8b
Mathews Reji <149689917+Mathews-Reji@users.noreply.github.com>
2024-07-10 18:25:00 +0530

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -3,6 +3,7 @@ import joblib
 # Load the model once when the module is imported
 try:
     model = joblib.load("06_07_lgbm_model.sav")
+    print("Model loaded successfully.")
 except Exception as e:
     model = None
     print(f"Error loading model: {e}")
